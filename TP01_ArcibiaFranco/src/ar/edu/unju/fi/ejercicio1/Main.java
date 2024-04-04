4fc9042de0f03390ad0c20ad29678167c93dee2b
Franco <francoarcibia423@gmail.com>
2024-04-04 17:14:56 -0300

    Punto 1 resuelto , asistencia clase

--- TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,13 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-
+		int num1 = 2;
+		int num2 = 4;
+		int num3 = 6;
+		int num4 = 8;
+		int num5 = 10;
+		double Promedio = (num1 + num2 + num3 + num4 + num5)/5;
+System.out.println("El Promedio es= " +Promedio);
 	}
 
 }
---
+++ TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio6.model;
+
+import java.time.LocalDate;
+
+public class Persona {
+
+private int dni;
+private String nombre;
+private LocalDate Fecha;
+private String provincia;
+
+
+}
+
