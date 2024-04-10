8872713a5e453eeff5c4b9b0691d40658a5703ec
Franco <francoarcibia423@gmail.com>
2024-04-10 00:16:29 -0300

    ejercicio 2 resuelto

--- TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -1,35 +1,20 @@
 package ar.edu.unju.fi.ejercicio2;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		long factorial = 1;
-		System.out.println("Ingrese numero :");
-		
-		
-		if(scanner.hasNextInt()) {
-			int num = scanner.nextInt();
-			if(num<0 || num >10) {
-				System.out.println("Solo calcula con numeros del 0 al 10 !");
-			}
-			else {
-				while(num!=0) {
-					factorial=factorial*num;
-					num--;
-				}
-				System.out.println("El FACTORIAL ES: "+factorial);
-				scanner.close();
-			}
-		}
-		else {
-			System.out.println("Se ingreso datos incorrectos");
-		}
-		
-		
+	String pais = "Argentina";
+	byte edad = 12 ;
+	double Altura = 100.2;
+	double precio = 17.50;
+	long numero = 388579716;
+	double calculo = Math.cos(0.5);
+	System.out.println("El pais es = " +pais);
+	System.out.println("La edad de una persona = " +edad);
+	System.out.println("La altura de un edificio es = " +Altura);
+	System.out.println("El precio de un producto de supermercado = " +precio);
+	System.out.println("El Numero del telefono es = " +numero);
+	System.out.println("El calculo de 0.5= " +calculo);
 	}
+
 }
