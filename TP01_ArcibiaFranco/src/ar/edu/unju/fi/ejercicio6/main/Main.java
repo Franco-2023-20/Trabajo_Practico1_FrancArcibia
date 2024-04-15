3634330d651e3a8853a3d6940918f96c9bbe63f3
Franco <francoarcibia423@gmail.com>
2024-04-15 20:07:38 -0300

    Resuelto el 6 ejercicio

--- TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,10 +1,33 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+
+import ar.edu.unju.fi.ejercicio6.model.Persona;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Persona persona1 = new Persona(11111,"Franco",LocalDate.of(2002,06,01),"Jujuy"); 
+	
+		publicarpersona(persona1);
+		Persona persona2 = new Persona();
+		persona2.setDni(22222);
+		persona2.setNombre("Caro");
+	    persona2.setFecha(LocalDate.of(2006,06,01));
+	    persona2.setProvincia("Salta");
+	    publicarpersona(persona2);
+		
+	}
+	public static void publicarpersona (Persona persona) {
+		System.out.println(persona.getDatos());	
 
 	}
-
+	
+	
 }
+	
+
+	
--- TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
+++ TP01_ArcibiaFranco/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -1,6 +1,8 @@
 package ar.edu.unju.fi.ejercicio6.model;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Persona {
 
@@ -8,7 +10,68 @@ private int dni;
 private String nombre;
 private LocalDate Fecha;
 private String provincia;
+public Persona() {
 
+}
+public Persona(int dni, String nombre, LocalDate fecha, String provincia) {
+	this.dni = dni;
+	this.nombre = nombre;
+	this.Fecha = fecha;
+	this.provincia = provincia;
+}
+public long getedad() {
+	LocalDate fechactual = LocalDate.now();
+	Period dias = Period.between(this.getFecha(), fechactual);
+	return dias.getYears();
+			}
+
+public String getmensaje () {
+	if (this.getedad()>17) {
+		String mensaje = "Es mayor de edad";
+return mensaje;
+	}	
+else { 
+		String mensaje = "Es menor de edad";
+		return mensaje;
+}
+	
+}
+public String getDatos() {
+return this.dni+" - "+this.nombre+" - "+this.Fecha+" - "+this.provincia+" - "+this.getedad()+ " - "+this.getmensaje();
+}
+
+public int getDni() {
+	return dni;
+}
+
+public void setDni(int dni) {
+	this.dni = dni;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public LocalDate getFecha() {
+	return Fecha;
+}
+
+public void setFecha(LocalDate fecha) {
+	Fecha = fecha;
+}
+
+public String getProvincia() {
+	return provincia;
+}
+
+public void setProvincia(String provincia) {
+	this.provincia = provincia;
+}
+	
 
 }
 
